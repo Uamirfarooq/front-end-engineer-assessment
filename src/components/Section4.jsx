d471f7d2dce148d8ff600fbc3c01d79b085790cf
Uamirfarooq <umair.codedeveloper@gmail.com>
2025-01-01 16:06:29 +0300

    section4 compelete

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -16,83 +16,196 @@ const Section4 = () => {
                 </div>
                 {/* Box 2 */}
                 <div className="w-[480px] h-[380px] border-t-[1px] border-gray-300  flex flex-col items-center justify-center bg-white shadow-md rounded-lg relative ">
-                <img
-                    src="../../public/Gabrun-Minutes-BG 1.png"
-                    alt="Logo"
-                    className=" h-[150px] absolute right-0 top-0 object-contain gap-0  z-30"
-                />
-                <img
-                    src="../../public/Group 10.png"
-                    alt="Logo"
-                    className=" h-[280px] absolute left-5 top-16   object-contain gap-0  z-30"
-                />
-                <img
-                    src="../../public/Group 8.png"
-                    alt="Logo"
-                    className=" h-[350px]  absolute z-50 top-8 object-contain gap-0 "
-                />
-                <img
-                    src="../../public/Group 9.png"
-                    alt="Logo"
-                    className=" h-[280px] absolute right-5 top-16  object-contain gap-0  z-30"
-                />
+                    <img
+                        src="../../public/Gabrun-Minutes-BG 1.png"
+                        alt="Logo"
+                        className=" h-[150px] absolute right-0 top-0 object-contain gap-0  z-30"
+                    />
+                    <img
+                        src="../../public/Group 10.png"
+                        alt="Logo"
+                        className=" h-[280px] absolute left-5 top-16   object-contain gap-0  z-30"
+                    />
+                    <img
+                        src="../../public/Group 8.png"
+                        alt="Logo"
+                        className=" h-[350px]  absolute z-50 top-8 object-contain gap-0 "
+                    />
+                    <img
+                        src="../../public/Group 9.png"
+                        alt="Logo"
+                        className=" h-[280px] absolute right-5 top-16  object-contain gap-0  z-30"
+                    />
 
                 </div>
                 {/* Box 3 */}
-                <div className="w-[480px] h-[380px] border-t-[1px] border-gray-300  flex flex-col items-center justify-center bg-white shadow-md rounded-lg">
-                    <h3 className="text-lg font-bold text-gray-800">Box 3</h3>
-                    <p className="text-sm text-gray-600">Description for box 3</p>
+                <div className="w-[480px] h-[380px] border-t-[1px] border-gray-300  flex flex-col items-center justify-center bg-white shadow-md rounded-lg relative">
+                    <img
+                        src="../../public/Gabrun-Minutes-BG 1.png"
+                        alt="Logo"
+                        className=" h-[150px] absolute left-0 top-0 object-contain gap-0  z-30"
+                    />
+
+                    <img
+                        src="../../public/Group 13.png"
+                        alt="Logo"
+                        className="w-[430px] absolute top-14  object-contain gap-0  z-30"
+                    />
+                    <img
+                        src="../../public/Group 12.png"
+                        alt="Logo"
+                        className="w-[400px]  absolute   object-contain gap-0  z-30"
+                    />
+                    <img
+                        src="../../public/Group 11.png"
+                        alt="Logo"
+                        className="w-[370px]  absolute bottom-14  object-contain gap-0  z-30"
+                    />
+                    <img
+                        src="../../public/solar_magnifer-outline.png"
+                        alt="Logo"
+                        className="  absolute right-5 bottom-5  object-contain gap-0  z-10"
+                    />
+
                 </div>
                 {/* Box 4 */}
                 <div className="w-[482px] flex flex-col justify-center">
                     <h2 className="text-4xl font-bold text-gray-900 mb-4">
-                    Automated Resume Screening & Filtering
+                        Automated Resume Screening & Filtering
                     </h2>
                     <p className="text-lg text-gray-600">
-                    Our AI instantly sorts and prioritizes resumes, highlighting only the strongest matches and discarding unqualified applicants.
+                        Our AI instantly sorts and prioritizes resumes, highlighting only the strongest matches and discarding unqualified applicants.
                     </p>
                 </div>
                 {/* Box 5 */}
                 <div className="w-[482px] flex flex-col justify-center">
                     <h2 className="text-4xl font-bold text-gray-900 mb-4">
-                    Online Assessment
+                        Online Assessment
                     </h2>
                     <p className="text-lg text-gray-600">
-                    Candidates complete a quick skill or behavioral test to validate their capabilities. This process is seamlessly integrated and fully automated.
+                        Candidates complete a quick skill or behavioral test to validate their capabilities. This process is seamlessly integrated and fully automated.
                     </p>
                 </div>
                 {/* Box 6 */}
-                <div className="w-[480px] h-[380px] border-t-[1px] border-gray-300  flex flex-col items-center justify-center bg-white shadow-md rounded-lg">
-                    <h3 className="text-lg font-bold text-gray-800">Box 6</h3>
-                    <p className="text-sm text-gray-600">Description for box 6</p>
+                <div className="w-[480px] h-[380px] border-t-[1px] border-gray-300  flex flex-col items-center justify-center bg-white shadow-md rounded-lg relative">
+                    <img
+                        src="../../public/Gabrun-Minutes-BG 1.png"
+                        alt="Logo"
+                        className=" h-[150px] absolute right-0 top-0 object-contain gap-0  z-30"
+                    />
+                    <div className=" border-t-[1px] border-gray-300  flex flex-col items-center justify-center bg-white shadow-md rounded-lg relative p-8 z-50">
+                        <div className="flex ">
+                            <div class="flex items-center justify-center w-14 h-14 ">
+                                <input
+                                    type="checkbox"
+                                    checked
+                                    className="w-5 h-5 text-green-600 rounded border-gray-300 focus:ring-green-500 focus:ring-2 accent-green-600"
+                                />
+
+                            </div>
+                            <div class="shrink w-64 h-12 px-4 flex items-center justify-start border border-gray-200 text-gray-400 font-medium line-through rounded-lg">
+                                1- Creative Design Skills Test
+                            </div>
+                        </div>
+                        <div className="flex ">
+                            <div class="flex items-center justify-center w-14 h-14 ">
+                                <input
+                                    type="checkbox"
+                                    checked
+                                    className="w-5 h-5 text-green-600 rounded border-gray-300 focus:ring-green-500 focus:ring-2 accent-green-600"
+                                />
+
+                            </div>
+                            <div class="shrink w-64 h-12 px-4 flex items-center justify-start border border-gray-200 text-gray-400 font-medium line-through rounded-lg">
+                                2- UX/UI Design Challenge
+                            </div>
+                        </div>
+                        <div className="flex ">
+                            <div class="flex items-center justify-center w-14 h-14 ">
+                                <input
+                                    type="checkbox"
+                                    checked
+                                    className="w-5 h-5 text-green-600 rounded border-gray-300 focus:ring-green-500 focus:ring-2 accent-green-600"
+                                />
+
+                            </div>
+                            <div class="shrink w-64 h-12 px-4 flex items-center justify-start border border-gray-200 text-gray-400 font-medium line-through rounded-lg">
+                                3- Problem-Solving Task
+                            </div>
+                        </div>
+                        <div className="flex ">
+                            <div class="flex items-center justify-center w-14 h-14 ">
+                                <input
+                                    type="checkbox"
+
+                                    className="w-5 h-5 text-green-600 rounded border-gray-300 focus:ring-green-500 focus:ring-2 accent-green-600"
+                                />
+
+                            </div>
+                            <div class="shrink w-64 h-12 px-4 flex items-center justify-start border border-gray-200  text-gray-800 font-medium rounded-lg">
+                                3- Problem-Solving Task
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 {/* Box 7 */}
-                <div className="w-[480px] h-[380px] border-t-[1px] border-gray-300  flex flex-col items-center justify-center bg-white shadow-md rounded-lg">
-                    <h3 className="text-lg font-bold text-gray-800">Box 7</h3>
-                    <p className="text-sm text-gray-600">Description for box 7</p>
+                <div className="w-[480px] h-[380px] border-t-[1px] border-gray-300  flex flex-col items-center justify-center bg-white shadow-md rounded-lg relative">
+                    <img
+                        src="../../public/Gabrun-Minutes-BG 1.png"
+                        alt="Logo"
+                        className=" h-[150px] absolute left-0 top-0 object-contain gap-0  z-30"
+                    />
+                    <img
+                        src="../../public/image.png"
+                        alt="Logo"
+                        className=" z-40 h-[300px]  absolute right-20  object-contain gap-0 "
+                    />
+                    <img
+                        src="../../public/Group 15.png"
+                        alt="Logo"
+                        className=" absolute z-50 right-[-20px] top-16"
+                    />
                 </div>
                 {/* Box 8 */}
                 <div className="w-[482px] flex flex-col justify-center">
                     <h2 className="text-4xl font-bold text-gray-900 mb-4">
-                    AI Video Interviews
+                        AI Video Interviews
                     </h2>
                     <p className="text-lg text-gray-600">
-                    An AI-driven virtual interviewer conducts dynamic, adaptive interviews, capturing both technical insights and soft skills. Everything is entirely hands-free.
+                        An AI-driven virtual interviewer conducts dynamic, adaptive interviews, capturing both technical insights and soft skills. Everything is entirely hands-free.
                     </p>
                 </div>
                 {/* Box 9 */}
                 <div className="w-[482px] flex flex-col justify-center">
                     <h2 className="text-4xl font-bold text-gray-900 mb-4">
-                    Final Shortlist for the Line Manager
+                        Final Shortlist for the Line Manager
                     </h2>
                     <p className="text-lg text-gray-600">
-                    Recruito presents a curated list of the top performers, complete with interview recordings and evaluations, so you can make a swift, confident hiring decision.
+                        Recruito presents a curated list of the top performers, complete with interview recordings and evaluations, so you can make a swift, confident hiring decision.
                     </p>
                 </div>
                 {/* Box 10 */}
-                <div className="w-[480px] h-[380px] border-t-[1px] border-gray-300  flex flex-col items-center justify-center bg-white shadow-md rounded-lg">
-                    <h3 className="text-lg font-bold text-gray-800">Box 10</h3>
-                    <p className="text-sm text-gray-600">Description for box 10</p>
+                <div className="w-[480px] h-[380px] border-t-[1px] border-gray-300  flex flex-col items-center justify-center bg-white shadow-md rounded-lg relative">
+                    <img
+                        src="../../public/Gabrun-Minutes-BG 1.png"
+                        alt="Logo"
+                        className=" h-[150px] absolute right-0 top-0 object-contain gap-0  z-30"
+                    />
+                    <img
+                        src="../../public/Group 16.png"
+                        alt="Logo"
+                        className=" absolute top-14  object-contain gap-0  z-30"
+                    />
+                    <img
+                        src="../../public/Group 17.png"
+                        alt="Logo"
+                        className=" absolute  top-36 object-contain gap-0  z-30"
+                    />
+                    <img
+                        src="../../public/Group 18.png"
+                        alt="Logo"
+                        className="absolute top-56     object-contain gap-0  z-30"
+                    />
                 </div>
             </div>
         </div>
